34e0502ab83aca5f2fe8189be94e8f4ea8c6652e
Yulia <yulia.bosher@gmail.com>
2025-12-19 00:03:25 +0000

    Default configurations

---
+++ backup.sh
@@ -0,0 +1,4 @@
+cp -r /var/ossec/etc/ossec.conf /home/yulia/Wazuh-Data-Exfiltration-and-Attack-Chain-Correlation
+cp -r /var/ossec/etc/rules/local_rules.xml /home/yulia/Wazuh-Data-Exfiltration-and-Attack-Chain-Correlation
+cp -r /var/ossec/etc/shared/default/agent.conf /home/yulia/Wazuh-Data-Exfiltration-and-Attack-Chain-Correlation
+
---
+++ restore.sh
@@ -0,0 +1,4 @@
+cp -r /home/yulia/Wazuh-Data-Exfiltration-and-Attack-Chain-Correlation/ossec.conf /var/ossec/etc/ossec.conf
+cp -r /home/yulia/Wazuh-Data-Exfiltration-and-Attack-Chain-Correlation/local_rules.xml /var/ossec/etc/rules/local_rules.xml
+cp -r /home/yulia/Wazuh-Data-Exfiltration-and-Attack-Chain-Correlation/agent.conf /var/ossec/etc/shared/default/agent.conf
+
